4dd7e218424c90cc4a1d9b5c9cb89cba7f64e5d0
simrandeep kaur <simrandeepkaur0119@gmail.com>
2024-01-03 14:29:55 +0530

    bgChanger, counter, toggleElement

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import BgChanger from './components/BgChanger.jsx'
+import Counter from './components/Counter.jsx'
+import Toggle from './components/Toggle.jsx'
+
+function App() {
+    return (
+        <>
+            {/* <BgChanger /> */}
+            < Counter />
+            {/* <Toggle /> */}
+        </>
+    )
+}
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import BgChanger from './components/BgChanger.jsx'
-import Counter from './components/Counter.jsx'
-import Toggle from './components/Toggle.jsx'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   < section className='w-screen h-screen flex items-center justify-center'>
     <React.StrictMode>
-      {/* <BgChanger /> */}
-      <Counter />
-      {/* <Toggle /> */}
+      <App />
     </React.StrictMode>
   </section>
 )
